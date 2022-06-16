ab7b20502a168e1cd06d5809f03bebcaa9aae5dc
Ing-yeo_Developer <sjw7324@naver.com>
2022-06-16 16:41:36 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import { Octokit, App } from "https://cdn.skypack.dev/octokit";
 
+const octokit = new Octokit({
+  auth: "personal-access-token123",
+});
+
 function Home() {
+  const test = async () => {
+    console.log(
+      console.log(
+        await octokit.request("GET /repos/{owner}/{repo}/pulls/comments", {
+          owner: "SeoJaeWan",
+          repo: "git-api",
+        }),
+        "ASDJKNAKSJD"
+      )
+    );
+  };
+
+  test();
   return <div className="Home">qwd</div>;
 }
 
