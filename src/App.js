13bb37abd3a1175b90b874f4555c4a9537ad0a75
Ing-yeo_Developer <sjw7324@naver.com>
2022-06-16 17:33:12 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import { Octokit, App } from "https://cdn.skypack.dev/octokit";
+import { useCallback, useEffect, useState } from "react";
 
 const octokit = new Octokit({
-  auth: "personal-access-token123",
+  auth: process.env.AUTH,
 });
 
-function Home() {
-  const test = async () => {
-    console.log(
-      console.log(
-        await octokit.request("GET /repos/{owner}/{repo}/pulls/comments", {
-          owner: "SeoJaeWan",
-          repo: "git-api",
-        }),
-        "ASDJKNAKSJD"
-      )
-    );
-  };
+const ONER = "SeoJaeWan";
+const REPO = "git-api";
 
-  test();
-  return <div className="Home">qwd</div>;
+function Home() {
+  const [data, setData] = useState([]);
+
+  const getPRDataHandler = useCallback(async () => {
+    // https://docs.github.com/en/rest/pulls/comments#about-the-pull-request-review-comments-api
+    const { data } = await octokit.request(
+      "GET /repos/{owner}/{repo}/pulls/comments",
+      {
+        owner: ONER,
+        repo: REPO,
+      }
+    );
+
+    setData(data);
+  }, []);
+
+  useEffect(() => {
+    getPRDataHandler();
+  }, []);
+
+  console.log(data);
+
+  return (
+    <div className="Home">
+      {data?.map((info) => {
+        return (
+          <div>
+            <p>작성자 : {info.user.login}</p>
+            <p>리뷰 : {info.body}</p>
+            <p>리뷰 경로 : {info.path}</p>
+            <a href={info.url}>바로가기</a>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Home;
